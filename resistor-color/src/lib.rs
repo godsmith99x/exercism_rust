8c66f0c2458df0189767721b925dd72132061338
godsmith <jgodfrey-smith@pm.me>
2022-01-30 16:14:02 -0500

    Finished

--- resistor-color/src/lib.rs
+++ resistor-color/src/lib.rs
@@ -12,17 +12,41 @@ pub enum ResistorColor {
     Yellow,
 }
 
+use ResistorColor::*;
+
 pub fn color_to_value(_color: ResistorColor) -> usize {
-    unimplemented!("convert a color into a numerical representation")
+    match _color {
+        Black => 0,
+        Brown => 1,
+        Red => 2,
+        Orange => 3,
+        Yellow => 4,
+        Green => 5,
+        Blue => 6,
+        Violet => 7,
+        Grey => 8,
+        White => 9,
+    }
 }
 
 pub fn value_to_color_string(value: usize) -> String {
-    unimplemented!(
-        "convert the value {} into a string representation of color",
-        value
-    )
+    match value {
+        0 => "Black".to_string(),
+        1 => "Brown".to_string(),
+        2 => "Red".to_string(),
+        3 => "Orange".to_string(),
+        4 => "Yellow".to_string(),
+        5 => "Green".to_string(),
+        6 => "Blue".to_string(),
+        7 => "Violet".to_string(),
+        8 => "Grey".to_string(),
+        9 => "White".to_string(),
+        _ => "value out of range".to_string(),
+    }
 }
 
 pub fn colors() -> Vec<ResistorColor> {
-    unimplemented!("return a list of all the colors ordered by resistance")
+    vec![
+        Black, Brown, Red, Orange, Yellow, Green, Blue, Violet, Grey, White,
+    ]
 }
