87852f50f6e06debb13c6cf1c800aa7e05f62513
godsmith <jgodfrey-smith@pm.me>
2022-02-01 15:11:44 -0500

    Finished Health Statistics

--- health-statistics/target/debug/deps/health_statistics-424fbf0c8d8f970e.d
+++
@@ -1,5 +0,0 @@
-/home/godsmith/snap/exercism/5/exercism/rust/health-statistics/target/debug/deps/health_statistics-424fbf0c8d8f970e.rmeta: tests/health-statistics.rs
-
-/home/godsmith/snap/exercism/5/exercism/rust/health-statistics/target/debug/deps/health_statistics-424fbf0c8d8f970e.d: tests/health-statistics.rs
-
-tests/health-statistics.rs:
--- health-statistics/target/rls/debug/deps/health_statistics-07a14c84926e17c9.d
+++
@@ -1,5 +0,0 @@
-/home/godsmith/snap/exercism/5/exercism/rust/health-statistics/target/rls/debug/deps/health_statistics-07a14c84926e17c9.rmeta: src/lib.rs
-
-/home/godsmith/snap/exercism/5/exercism/rust/health-statistics/target/rls/debug/deps/health_statistics-07a14c84926e17c9.d: src/lib.rs
-
-src/lib.rs:
