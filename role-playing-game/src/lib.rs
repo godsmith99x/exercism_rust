b722e3c28b66c0f133e4e95a04c2fcd8720d6fc8
godsmith <jgodfrey-smith@pm.me>
2022-02-02 15:02:23 -0500

    Completed Role Playing Game

--- role-playing-game/src/lib.rs
+++ role-playing-game/src/lib.rs
@@ -10,10 +10,37 @@ pub struct Player {
 
 impl Player {
     pub fn revive(&self) -> Option<Player> {
-        unimplemented!("Revive this player")
+        match self.health {
+            0 if self.level < 10 => Some(Player {
+                health: 100,
+                mana: None,
+                level: self.level,
+            }),
+            0 if self.level >= 10 => Some(Player {
+                health: 100,
+                mana: Some(100),
+                level: self.level,
+            }),
+            _ => None,
+        }
     }
 
     pub fn cast_spell(&mut self, mana_cost: u32) -> u32 {
-        unimplemented!("Cast a spell of cost {}", mana_cost)
+        match (self.mana, mana_cost) {
+            (None, mc) => {
+                if self.health >= mc {
+                    self.health -= mc;
+                } else {
+                    self.health = 0;
+                }
+                0
+            }
+            (Some(m), mc) if m < mc => 0,
+            (Some(m), mc) if m >= mc => {
+                self.mana = Some(m - mc);
+                2 * mc
+            }
+            (Some(_), _) => 0,
+        }
     }
 }
