3d388301388f999f7d47a6104f4bcc7c92561b40
godsmith <jgodfrey-smith@pm.me>
2022-02-02 13:06:30 -0500

    Completed Low-power Embedded Game

--- low-power-embedded-game/src/lib.rs
+++ low-power-embedded-game/src/lib.rs
@@ -3,19 +3,17 @@
 #![allow(unused)]
 
 pub fn divmod(dividend: i16, divisor: i16) -> (i16, i16) {
-    unimplemented!("implement `fn divmod`");
+    let ans = (dividend / divisor, dividend % divisor);
+    ans
 }
 
 pub fn evens<T>(iter: impl Iterator<Item = T>) -> impl Iterator<Item = T> {
-    unimplemented!("implement `fn evens`");
-    // TODO: remove this; it's only necessary to allow this function to compile
-    // before the student has done any work.
-    std::iter::empty()
+    iter.enumerate().filter(|(i, v)| i % 2 == 0).map(|(i, v)| v)
 }
 
 pub struct Position(pub i16, pub i16);
 impl Position {
     pub fn manhattan(&self) -> i16 {
-        unimplemented!("implement `fn manhattan`")
+        self.0.abs() + self.1.abs()
     }
 }
