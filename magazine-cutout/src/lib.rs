dba0a149c7e8267acd41272e6e7d121258eb546d
godsmith <jgodfrey-smith@pm.me>
2022-02-02 16:35:51 -0500

    Completed Magazine Cutout

--- magazine-cutout/src/lib.rs
+++ magazine-cutout/src/lib.rs
@@ -5,5 +5,17 @@
 use std::collections::HashMap;
 
 pub fn can_construct_note(magazine: &[&str], note: &[&str]) -> bool {
-    unimplemented!()
+    let mut words: HashMap<&str, i32> = HashMap::new();
+
+    for word in note {
+        let counter = words.entry(word).or_insert(0);
+        *counter += 1;
+    }
+
+    for word in magazine {
+        let counter = words.entry(word).or_insert(0);
+        *counter -= 1;
+    }
+
+    !words.values().any(|x| *x > 0)
 }
