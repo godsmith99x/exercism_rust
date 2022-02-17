45caa823b5ecdec98ce42785833d1c8da7ff6372
godsmith <jgodfrey-smith@pm.me>
2022-02-17 15:48:47 -0500

    Refactor

--- rpn-calculator/src/lib.rs
+++ rpn-calculator/src/lib.rs
@@ -9,6 +9,24 @@ pub enum CalculatorInput {
 
 use CalculatorInput::{Add, Divide, Multiply, Subtract, Value};
 
+pub fn evaluate(inputs: &[CalculatorInput]) -> Option<i32> {
+    let mut stack: Vec<i32> = Vec::new();
+
+    for input in inputs {
+        match input {
+            Value(num) => stack.push(*num),
+            _ if stack.len() < 2 => return None,
+            _ => operation(&mut stack, &input),
+        }
+    }
+
+    if stack.len() == 1 {
+        Some(stack[0])
+    } else {
+        None
+    }
+}
+
 fn operation(stack: &mut Vec<i32>, input: &CalculatorInput) {
     let num2 = stack.pop().unwrap();
     let num1 = stack.pop().unwrap();
@@ -20,20 +38,4 @@ fn operation(stack: &mut Vec<i32>, input: &CalculatorInput) {
         Divide => num1 / num2,
         _ => unreachable!(),
     });
-}
-pub fn evaluate(inputs: &[CalculatorInput]) -> Option<i32> {
-    let mut stack = Vec::new();
-
-    for input in inputs {
-        match input {
-            Value(num) => stack.push(*num),
-            _ if stack.len() < 2 => return None,
-            _ => operation(&mut stack, &input),
-        }
-    }
-    if stack.len() == 1 {
-        Some(stack[0])
-    } else {
-        None
-    }
 }
