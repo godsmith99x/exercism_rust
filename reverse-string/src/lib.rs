328cbaca8b40fc0dac8d37fee3dee99a1a76c8ab
godsmith <jgodfrey-smith@pm.me>
2022-02-23 12:11:25 -0500

    Refactor to satisfy exercism.org testrunner

--- reverse-string/src/lib.rs
+++ reverse-string/src/lib.rs
@@ -1,6 +1,5 @@
 use unicode_segmentation::UnicodeSegmentation;
 
 pub fn reverse(input: &str) -> String {
-    let reversed = input.graphemes(true).rev().collect();
-    reversed
+    input.graphemes(true).rev().collect()
 }
