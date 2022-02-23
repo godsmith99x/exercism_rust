6d85b2f3b6bf9175206640b026a5693c90335970
godsmith <jgodfrey-smith@pm.me>
2022-02-23 13:15:03 -0500

    Completed gigasecond

--- gigasecond/src/lib.rs
+++ gigasecond/src/lib.rs
@@ -1,6 +1,11 @@
+use time::Duration;
 use time::PrimitiveDateTime as DateTime;
 
 // Returns a DateTime one billion seconds after start.
 pub fn after(start: DateTime) -> DateTime {
-    unimplemented!("What time is a gigasecond later than {}", start);
+    let gigasecond = Duration::new(1_000_000_000, 0);
+
+    start
+        .checked_add(gigasecond)
+        .expect("Invalid start datetime entered")
 }
