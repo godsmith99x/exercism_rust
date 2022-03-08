d1e7c2f7989dfc1e8d76e456a6787f0a78686e2b
godsmith <jgodfrey-smith@pm.me>
2022-03-08 17:16:20 -0500

    Completed anagram

--- anagram/src/lib.rs
+++ anagram/src/lib.rs
@@ -1,9 +1,22 @@
 use std::collections::HashSet;
+use unicode_segmentation::UnicodeSegmentation;
 
-pub fn anagrams_for<'a>(word: &str, possible_anagrams: &[&str]) -> HashSet<&'a str> {
-    unimplemented!(
-        "For the '{}' word find anagrams among the following words: {:?}",
-        word,
-        possible_anagrams
-    );
+pub fn anagrams_for<'a>(word: &str, possible_anagrams: &'a [&str]) -> HashSet<&'a str> {
+    let word_lower = word.to_lowercase();
+    let word_sorted = sort(&word_lower);
+
+    possible_anagrams
+        .iter()
+        .filter(|sus| {
+            let sus_lower = sus.to_lowercase();
+            word_lower != sus_lower && word_sorted == sort(&sus_lower)
+        })
+        .copied()
+        .collect() // collect() will automatically infer the correct collection type based off the function return type
+}
+
+fn sort(word: &str) -> Vec<&str> {
+    let mut word_sorted = word.graphemes(true).collect::<Vec<&str>>();
+    word_sorted.sort_unstable();
+    word_sorted
 }
