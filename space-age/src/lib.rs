c96c0d62e00f62a30e1d051f012bb0f4f046364d
Godfrey-Smith, Joel [USA] <Godfrey-Smith_Joel@bah.com>
2022-11-28 13:44:24 -0500

    Completed space-age exercise

--- space-age/src/lib.rs
+++ space-age/src/lib.rs
@@ -2,20 +2,26 @@
 // In order to pass the tests you can add-to or change any of this code.
 
 #[derive(Debug)]
-pub struct Duration;
+pub struct Duration {
+    seconds: u64,
+}
 
 impl From<u64> for Duration {
     fn from(s: u64) -> Self {
-        unimplemented!("s, measured in seconds: {}", s)
+        Duration { seconds: s }
     }
 }
 
 pub trait Planet {
+    const EARTH_ORBITAL_PERIOD: f64 = 31557600.0;
+    const ORBITAL_RATIO: f64;
+
+    fn orbital_period() -> f64 {
+        Self::EARTH_ORBITAL_PERIOD * Self::ORBITAL_RATIO
+    }
+
     fn years_during(d: &Duration) -> f64 {
-        unimplemented!(
-            "convert a duration ({:?}) to the number of years on this planet for that duration",
-            d,
-        );
+        d.seconds as f64 / Self::orbital_period()
     }
 }
 
@@ -28,11 +34,11 @@ pub struct Saturn;
 pub struct Uranus;
 pub struct Neptune;
 
-impl Planet for Mercury {}
-impl Planet for Venus {}
-impl Planet for Earth {}
-impl Planet for Mars {}
-impl Planet for Jupiter {}
-impl Planet for Saturn {}
-impl Planet for Uranus {}
-impl Planet for Neptune {}
+impl Planet for Mercury {const ORBITAL_RATIO: f64 =  0.2408467;}
+impl Planet for Venus {const ORBITAL_RATIO: f64 =  0.61519726;}
+impl Planet for Earth {const ORBITAL_RATIO: f64 =  1.0;}
+impl Planet for Mars {const ORBITAL_RATIO: f64 = 1.8808158;}
+impl Planet for Jupiter {const ORBITAL_RATIO: f64 =  11.862615;}
+impl Planet for Saturn {const ORBITAL_RATIO: f64 =  29.447498;}
+impl Planet for Uranus {const ORBITAL_RATIO: f64 =  84.016846;}
+impl Planet for Neptune {const ORBITAL_RATIO: f64 =  164.79132;}
