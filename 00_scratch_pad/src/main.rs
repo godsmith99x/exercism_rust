a26baf9dd00419e2b0be5e956396cb1d3c1a78aa
Godfrey-Smith, Joel [USA] <Godfrey-Smith_Joel@bah.com>
2023-01-11 11:52:24 -0500

    Finished steps to calculate valid neighbors

--- 00_scratch_pad/src/main.rs
+++ 00_scratch_pad/src/main.rs
@@ -25,7 +25,6 @@ fn main() {
     let working_case4: Vec<Vec<char>> = create_2d_vec(test_case4);
     let working_case5: Vec<Vec<char>> = create_2d_vec(test_case5);
 
-
     //2. Search each cell of the 2D array for "*"s and record their indices
     fn locate_mines(working_vec: &Vec<Vec<char>>) -> Vec<(i32, i32)> {
         let mut mine_indicies: Vec<(i32, i32)> = Vec::new();
@@ -45,47 +44,6 @@ fn main() {
 
     //3. Calculate the neighbor coordinates for each mine
 
-
-    fn calculate_neighbors(mine_index: (i32, i32)) -> Vec<(i32, i32)> {
-
-        fn add_indicies(point1: &(i32, i32), point2: &(i32, i32)) -> (i32, i32) {
-            let row_translation = point1.0 + point2.0;
-            let col_translation = point1.1 + point2.1;
-            let new_point = (row_translation, col_translation);
-            new_point
-        }
-
-        let neighbor_template: Vec<(i32, i32)> = vec![
-            (-1, -1),
-            (-1, 0),
-            (-1, 1),
-            (0, -1),
-            (0, 1),
-            (1, -1),
-            (1, 0),
-            (1, 1),
-        ];
-
-        let neighbors_working = neighbor_template
-            .iter()
-            .map(|&t| add_indicies(&t, &mine_index))
-            .collect();
-
-        neighbors_working
-    }
-
-    // fn locate_neighbors(mine_indicies: Vec<(i32, i32)>, row_len: &i32, col_len: &i32) -> Vec<(i32, i32)> {
-    //     let mut neighbors_vec: Vec<(i32, i32)> = Vec::new();
-
-    //     let valid_neighbors = mine_indicies.iter().filter(||);
-    //     // for mine_index in mine_indicies {
-    //     //     let mut neighbors = calculate_neighbors(mine_index);
-    //     //     neighbors_vec.append(&mut neighbors);
-    //     // }
-
-    //     neighbors_vec
-    // }
-
     fn find_row_len(working_vec: &Vec<Vec<char>>) -> i32 {
         let row_len: i32 = working_vec[0].len() as i32;
         row_len
@@ -99,9 +57,58 @@ fn main() {
     let row_len: i32 = find_row_len(&working_case5);
     let col_len: i32 = find_col_len(&working_case5);
 
+    fn add_indicies(point1: &(i32, i32), point2: &(i32, i32)) -> (i32, i32) {
+        let row_translation = point1.0 + point2.0;
+        let col_translation = point1.1 + point2.1;
+        let new_point = (row_translation, col_translation);
+        new_point
+    }
+
+    fn is_valid(point: &(i32, i32), row_len: &i32, col_len: &i32) -> bool {
+        match point {
+            point if point.0 < 0 => false,
+            point if point.0 > *row_len => false,
+            point if point.1 < 0 => false,
+            point if point.1 > *col_len => false,
+            _ => true,
+        }
+    }
+
+    fn calculate_neighbors(
+        mine_index: &(i32, i32),
+        row_len: &i32,
+        col_len: &i32,
+    ) -> Vec<(i32, i32)> {
+        let neighbor_template: Vec<(i32, i32)> = vec![
+            (-1, -1),
+            (-1, 0),
+            (-1, 1),
+            (0, -1),
+            (0, 1),
+            (1, -1),
+            (1, 0),
+            (1, 1),
+        ];
+
+        let neighbors_working: Vec<(i32, i32)> = neighbor_template
+            .iter()
+            .map(|&p| add_indicies(&p, &mine_index))
+            .collect();
+
+        let mut neighbors_valid: Vec<(i32, i32)> = Vec::new();
+
+        for point in neighbors_working {
+            if is_valid(&point, &row_len, &col_len) {
+                neighbors_valid.push(point.clone())
+            };
+        }
+
+        neighbors_valid
+    }
+
     println!("{:#?} \n", row_len);
     println!("{:#?} \n", col_len);
-    println!("{:#?} \n", calculate_neighbors(mine_locations[0]));
+    println!("{:#?} \n", calculate_neighbors(&mine_locations[0], &row_len, &col_len));
     // println!("{:#?} \n", working_case2);
     // println!("{:#?} \n", working_case3);
     // println!("{:#?} \n", working_case4);
